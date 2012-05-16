9bac8b97bcad5c4633a421745d02f0d295f0363b
Jeremy Neander <subjectego@gmail.com>
2012-05-16 01:22:29 -0500

    added: methods to Game class to find blocking positions

--- src/com/jneander/tictactoe/Game.java
+++ src/com/jneander/tictactoe/Game.java
@@ -41,10 +41,38 @@ public class Game {
       } else {
         mark = new Mark( 0, 0, MarkType.COMPUTER );
       }
+    } else {
+      for ( int row = 0; row < gameBoard.length; row++ ) {
+        if ( setNeedsBlock( gameBoard[row] ) )
+          mark = new Mark( row, findBlockPosition( gameBoard[row] ), MarkType.COMPUTER );
+      }
     }
 
     gameBoard[mark.row][mark.col] = mark.markType;
     lastMark = mark;
+  }
+
+  private boolean setNeedsBlock( MarkType marks[] ) {
+    int blankCount = 0, playerCount = 0;
+
+    for ( MarkType mark : marks ) {
+      if ( mark == MarkType.PLAYER )
+        playerCount++;
+      else if ( mark == MarkType.BLANK )
+        blankCount++;
+    }
+
+    return (playerCount == 2 && blankCount == 1);
+  }
+
+  private int findBlockPosition( MarkType marks[] ) {
+    int position = -1;
+
+    for ( int pos = 0; pos < marks.length; pos++ )
+      if ( marks[pos] == MarkType.BLANK )
+        position = pos;
+
+    return position;
   }
 
   private boolean isCenterMark( Mark mark ) {
