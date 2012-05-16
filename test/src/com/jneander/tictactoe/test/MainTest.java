4d22d0ba2cc942e5e04e5ee8631a21b6442c5c92
Jeremy Neander <subjectego@gmail.com>
2012-05-15 20:47:47 -0500

    added: 'testAllSpacesOpen()' method to MainTest class

--- test/src/com/jneander/tictactoe/test/MainTest.java
+++ test/src/com/jneander/tictactoe/test/MainTest.java
@@ -2,9 +2,18 @@ package com.jneander.tictactoe.test;
 
 import junit.framework.TestCase;
 
+import com.jneander.tictactoe.Game;
+
 public class MainTest extends TestCase {
+  private Game game;
 
   protected void setUp() throws Exception {
     super.setUp();
+    
+    game = new Game();
+  }
+  
+  public void testAllSpacesOpen() {
+    
   }
 }
