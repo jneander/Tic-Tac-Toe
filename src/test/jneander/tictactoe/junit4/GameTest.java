1b59cc8d6f30976ef4c3225398e88f3f77de37b6
Jeremy Neander <subjectego@gmail.com>
2012-06-10 22:17:44 -0500

    created: 'GameTest' test class features tests for the 'Game' class

---
+++ src/test/jneander/tictactoe/junit4/GameTest.java
@@ -0,0 +1,70 @@
+package test.jneander.tictactoe.junit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.jneander.tictactoe.game.Game;
+import com.jneander.tictactoe.game.Mark;
+
+public class GameTest {
+  private Game game;
+  private final int boardSize = 3;
+  private final int indexCount = boardSize * boardSize;
+
+  @Before
+  public void setUp() {
+    game = new Game();
+  }
+
+  @Test
+  public void newGameHasNoWinner() {
+    assertEquals( Mark.BLANK, game.getWinner() );
+  }
+
+  @Test
+  public void newGameIsNotOver() {
+    assertFalse( game.isGameOver() );
+  }
+
+  @Test
+  public void newGameHasNoMarks() {
+    for ( int spaceIndex = 0; spaceIndex < indexCount; spaceIndex++ )
+      assertFalse( game.positionIsMarked( spaceIndex ) );
+  }
+
+  @Test
+  public void canMakePlayerMark() {
+    game.makePlayerMarkAtPosition( 0 );
+    assertTrue( game.positionIsMarked( 0 ) );
+
+    game.makePlayerMarkAtPosition( 8 );
+    assertTrue( game.positionIsMarked( 8 ) );
+  }
+
+  @Test
+  public void canMakeComputerMark() {
+    boolean foundComputerMark = false;
+
+    game.makeComputerMark();
+    for ( int spaceIndex = 0; spaceIndex < indexCount; spaceIndex++ )
+      foundComputerMark |= game.positionIsMarked( spaceIndex );
+
+    assertTrue( foundComputerMark );
+  }
+
+  @Test
+  public void gameCanEnd() {
+    for ( int move = 0; move < 3; move++ )
+      game.makeComputerMark();
+    assertTrue( game.isGameOver() );
+    assertEquals( Mark.COMPUTER, game.getWinner() );
+  }
+
+  @Test
+  public void gameCanReset() {
+    game.makePlayerMarkAtPosition( 0 );
+    game.reset();
+    assertFalse( game.positionIsMarked( 0 ) );
+  }
+}
