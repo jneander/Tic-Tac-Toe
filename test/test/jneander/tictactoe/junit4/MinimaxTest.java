d9a8fbce4cfecb1cc7754f21c451319fe3fb8b66
Jeremy Neander <subjectego@gmail.com>
2012-06-13 22:29:03 -0500

    changed: field name for 'Board' object now reflects the size of the board

--- test/test/jneander/tictactoe/junit4/MinimaxTest.java
+++ test/test/jneander/tictactoe/junit4/MinimaxTest.java
@@ -10,45 +10,45 @@ import com.jneander.tictactoe.game.Mark;
 import com.jneander.tictactoe.util.Minimax;
 
 public class MinimaxTest {
-  private Board board;
+  private Board board3x3;
 
   @Before
   public void setUp() {
-    board = new Board(3);
+    board3x3 = new Board(3);
   }
 
   @Test
   public void scoreHighOnWinningMove() {
-    board.addMark( 0, Mark.COMPUTER );
-    board.addMark( 2, Mark.PLAYER );
-    board.addMark( 3, Mark.COMPUTER );
-    board.addMark( 5, Mark.PLAYER );
-    board.addMark( 6, Mark.COMPUTER );
+    board3x3.addMark( 0, Mark.COMPUTER );
+    board3x3.addMark( 2, Mark.PLAYER );
+    board3x3.addMark( 3, Mark.COMPUTER );
+    board3x3.addMark( 5, Mark.PLAYER );
+    board3x3.addMark( 6, Mark.COMPUTER );
     
-    assertEquals(1, Minimax.minimax(board, Mark.PLAYER));
+    assertEquals(1, Minimax.minimax(board3x3, Mark.PLAYER));
   }
 
   @Test
   public void scoreLowOnLosingMove() {
-    board.addMark( 2, Mark.PLAYER );
-    board.addMark( 0, Mark.COMPUTER );
-    board.addMark( 5, Mark.PLAYER );
-    board.addMark( 3, Mark.COMPUTER );
+    board3x3.addMark( 2, Mark.PLAYER );
+    board3x3.addMark( 0, Mark.COMPUTER );
+    board3x3.addMark( 5, Mark.PLAYER );
+    board3x3.addMark( 3, Mark.COMPUTER );
     
-    assertEquals(-1, Minimax.minimax(board, Mark.PLAYER));
+    assertEquals(-1, Minimax.minimax(board3x3, Mark.PLAYER));
   }
 
   @Test
   public void scoreMidOnTyingMove() {
-    board.addMark( 4, Mark.PLAYER );
-    board.addMark( 0, Mark.COMPUTER );
-    board.addMark( 6, Mark.PLAYER );
-    board.addMark( 2, Mark.COMPUTER );
-    board.addMark( 1, Mark.PLAYER );
-    board.addMark( 7, Mark.COMPUTER );
-    board.addMark( 5, Mark.PLAYER );
-    board.addMark( 3, Mark.COMPUTER );
+    board3x3.addMark( 4, Mark.PLAYER );
+    board3x3.addMark( 0, Mark.COMPUTER );
+    board3x3.addMark( 6, Mark.PLAYER );
+    board3x3.addMark( 2, Mark.COMPUTER );
+    board3x3.addMark( 1, Mark.PLAYER );
+    board3x3.addMark( 7, Mark.COMPUTER );
+    board3x3.addMark( 5, Mark.PLAYER );
+    board3x3.addMark( 3, Mark.COMPUTER );
     
-    assertEquals(0, Minimax.minimax(board, Mark.PLAYER));
+    assertEquals(0, Minimax.minimax(board3x3, Mark.PLAYER));
   }
 }
