17d05ab97891f4faa68bbba13ec4a7b1430373d0
Jeremy Neander <subjectego@gmail.com>
2012-06-13 23:11:31 -0500

    added: 'boardCanDetectWinningSolution()' and 'boardCanFindWinningMark()' tests have been ported to test a larger board; test names have been suffixed with the appropriate board size

--- test/test/jneander/tictactoe/junit4/BoardTest.java
+++ test/test/jneander/tictactoe/junit4/BoardTest.java
@@ -10,23 +10,25 @@ import com.jneander.tictactoe.game.Mark;
 
 public class BoardTest {
   private Board board3x3;
+  private Board board4x4;
 
   @Before
   public void setUp() {
-    board3x3 = new Board(3);
+    board3x3 = new Board( 3 );
+    board4x4 = new Board( 4 );
   }
-  
+
   @Test
   public void canGetAvailableSpaces() {
-    assertEquals(9, board3x3.getAvailableSpaces().length);
-    
+    assertEquals( 9, board3x3.getAvailableSpaces().length );
+
     board3x3.addMark( 0, Mark.PLAYER );
-    assertEquals(8, board3x3.getAvailableSpaces().length);
-    assertEquals(1, board3x3.getAvailableSpaces()[0]);
-    
+    assertEquals( 8, board3x3.getAvailableSpaces().length );
+    assertEquals( 1, board3x3.getAvailableSpaces()[0] );
+
     board3x3.addMark( 5, Mark.PLAYER );
-    assertEquals(7, board3x3.getAvailableSpaces().length);
-    assertEquals(6, board3x3.getAvailableSpaces()[4]);
+    assertEquals( 7, board3x3.getAvailableSpaces().length );
+    assertEquals( 6, board3x3.getAvailableSpaces()[4] );
   }
 
   @Test
@@ -56,7 +58,7 @@ public class BoardTest {
   }
 
   @Test
-  public void boardCanDetectWinningSolution() {
+  public void boardCanDetectWinningSolution3x3() {
     board3x3.addMark( 0, Mark.COMPUTER );
     board3x3.addMark( 1, Mark.COMPUTER );
     assertFalse( board3x3.hasWinningSolution() );
@@ -66,12 +68,34 @@ public class BoardTest {
   }
 
   @Test
-  public void boardCanFindWinningMark() {
+  public void boardCanDetectWinningSolution4x4() {
+    board4x4.addMark( 0, Mark.COMPUTER );
+    board4x4.addMark( 1, Mark.COMPUTER );
+    board4x4.addMark( 2, Mark.COMPUTER );
+    assertFalse( board4x4.hasWinningSolution() );
+
+    board4x4.addMark( 3, Mark.COMPUTER );
+    assertTrue( board4x4.hasWinningSolution() );
+  }
+
+  @Test
+  public void boardCanFindWinningMark3x3() {
     board3x3.addMark( 0, Mark.COMPUTER );
     board3x3.addMark( 1, Mark.COMPUTER );
     board3x3.addMark( 2, Mark.COMPUTER );
 
     assertTrue( board3x3.hasWinningSolution() );
     assertTrue( board3x3.getWinningMark() == Mark.COMPUTER );
+  }
+
+  @Test
+  public void boardCanFindWinningMark4x4() {
+    board4x4.addMark( 0, Mark.COMPUTER );
+    board4x4.addMark( 1, Mark.COMPUTER );
+    board4x4.addMark( 2, Mark.COMPUTER );
+    board4x4.addMark( 3, Mark.COMPUTER );
+
+    assertTrue( board4x4.hasWinningSolution() );
+    assertTrue( board4x4.getWinningMark() == Mark.COMPUTER );
   }
 }
